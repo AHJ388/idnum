b13ac9ce1ba929159600c2e80cf32a7e6927f72d
AHJ388 <amarchandhooda40@gmail.com>
2022-11-27 16:15:28 +0530

    Completed

---
+++ src/Components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='container my-5 fs-1'>About</div>
+  )
+}
+
+export default About
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container my-5 fs-1'>
+         This is a basic website and there are two buttons available. While clicking on the increase button it'll increase the number and While clicking on the decrease button it'll decrease the number. <strong>That's it.</strong> 
+        </div>
+  )
+}
+
+export default Home 
